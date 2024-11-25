d7f9d149897f126318e0343ed1806b8f4ee3fcfb
Benjamin Beilharz <pixelsandpointers@pm.me>
2024-11-25 15:41:44 +0100

    added camera from learnopengl

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,22 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 // clang-format on
+#include "glm/ext/matrix_clip_space.hpp"
+#include "glm/ext/matrix_transform.hpp"
+
+#include <filesystem>
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 #include <iostream>
 
-void glfw_error_callback([[maybe_unused]] int error, char const* desciption)
+#include "renderer/Camera.hpp"
+#include "renderer/Model.hpp"
+#include "renderer/Shader.hpp"
+
+void glfw_error_callback([[maybe_unused]] int error, char const* description)
 {
-    std::cout << "glfw error: " << desciption << "\n";
+    std::cout << "glfw error: " << description << "\n";
 }
 
 int main()
@@ -26,7 +34,10 @@ int main()
     glfwWindowHintString(GLFW_X11_CLASS_NAME, "3d");
     glfwWindowHintString(GLFW_WAYLAND_APP_ID, "3d");
 
-    auto* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    constexpr int window_width = 640;
+    constexpr int window_height = 480;
+
+    auto* window = glfwCreateWindow(window_width, window_height, "Hello World", nullptr, nullptr);
     if (!window) {
         std::cout << "glfwCreateWindow() failed\n";
         glfwTerminate();
@@ -51,9 +62,34 @@ int main()
     ImGui_ImplGlfw_InitForOpenGL(window, true); // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
     ImGui_ImplOpenGL3_Init();
 
+    // assuming we set the CWD to root
+    auto path = std::filesystem::current_path();
+    std::cout << path.c_str() << std::endl;
+    path.append("assets/objects/backpack/backpack.obj");
+
+    Camera camera{glm::vec3(0.f, 0.f, -3.f)};
+    Model obj{path.string()};
+    Shader shader("src/shader/obj.vert", "src/shader/obj.frag");
+
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);
+
+        shader.use();
+
+        // view/projection transformations
+        glm::mat4 projection = glm::perspective(glm::radians(camera.m_zoom), static_cast<float>(window_width) / static_cast<float>(window_height), 0.1f, 100.0f);
+        glm::mat4 view = camera.get_view_matrix();
+        shader.set_mat4("projection", projection);
+        shader.set_mat4("view", view);
+
+        // render the loaded model
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f)); // it's a bit too big for our scene, so scale it down
+        shader.set_mat4("model", model);
+        obj.draw(shader);
 
         // Start the Dear ImGui frame
         ImGui_ImplOpenGL3_NewFrame();
