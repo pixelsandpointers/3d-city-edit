82ca37b99b7645bdee6060eef631aa49f31cd758
Moritz Vogel <moritzv7@gmail.com>
2024-12-02 16:13:53 +0100

    build: Remove workarounds from flake, add darwin

--- flake.nix
+++ flake.nix
@@ -2,43 +2,40 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    let
+      lib = nixpkgs.lib;
+      forAllSupportedSystems = f:
+        lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ] (system: f nixpkgs.legacyPackages.${system});
     in {
-      formatter.x86_64-linux = pkgs.nixfmt-classic;
+      formatter = forAllSupportedSystems (pkgs: pkgs.nixfmt-classic);
 
-      packages.x86_64-linux.default = pkgs.clang18Stdenv.mkDerivation {
-        name = "3d";
-        src = ./.;
-        nativeBuildInputs = with pkgs; [ cmake ninja pkg-config ];
-        buildInputs = with pkgs; [
-          assimp
-          glm
-          glfw
-          imgui
+      packages = forAllSupportedSystems (pkgs: {
+        default = pkgs.clang18Stdenv.mkDerivation {
+          name = "3d";
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ cmake ninja pkg-config ];
+          buildInputs = with pkgs; [
+            assimp
+            glm
+            glfw
+            (imgui.override { IMGUI_BUILD_GLFW_BINDING = true; })
+          ];
+        };
+      });
 
-          libGL
-          libffi
-          libxkbcommon
-          wayland
-          wayland-protocols
-          wayland-scanner
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXext
-          xorg.libXi
-          xorg.libXinerama
-          xorg.libXrandr
-        ];
-      };
-
-      devShells.x86_64-linux.default =
-        self.packages.x86_64-linux.default.overrideAttrs (old: {
+      devShells = forAllSupportedSystems (pkgs: {
+        default = self.packages.${pkgs.system}.default.overrideAttrs (old: {
           nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.ccache ];
           ASAN_OPTIONS = "symbolize=1";
           ASAN_SYMBOLIZER_PATH = "${pkgs.llvm}/bin/llvm-symbolizer";
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath old.buildInputs;
           CMAKE_BUILD_TYPE = "Debug";
           hardeningDisable = [ "fortify" ];
         });
+      });
     };
 }
