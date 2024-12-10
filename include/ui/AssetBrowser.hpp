6958d97881ac0383524462b9b4d08a2898f60a5a
Moritz Vogel <moritzv7@gmail.com>
2024-12-10 18:42:48 +0100

    ui: Add placeholder preview ChildWindow

--- include/ui/AssetBrowser.hpp
+++ include/ui/AssetBrowser.hpp
@@ -1,5 +1,14 @@
 #pragma once
 
+#include "core/Project.hpp"
+
+struct Node;
+
 struct AssetBrowser {
     void render();
+
+private:
+    Node const* m_selected_node{nullptr};
+    void traverse_model(Node const&);
+    void traverse_directory(FSCacheNode const&);
 };
--- src/ui/AssetBrowser.cpp
+++ src/ui/AssetBrowser.cpp
@@ -1,5 +1,4 @@
 #include "ui/AssetBrowser.hpp"
-#include "core/Project.hpp"
 #include <filesystem>
 #include <imgui.h>
 #include <iostream>
@@ -8,28 +7,35 @@ constexpr auto imgui_color_red = ImVec4{1.0f, 0.0f, 0.0f, 1.0f};
 constexpr auto imgui_color_grey = ImVec4{0.7f, 0.7f, 0.7f, 1.0f};
 constexpr auto imgui_color_light_grey = ImVec4{0.3f, 0.3f, 0.3f, 1.0f};
 
-void traverse_model(Node const& node)
+void AssetBrowser::traverse_model(Node const& node)
 {
     ImGui::PushStyleColor(ImGuiCol_Text, imgui_color_grey);
     for (auto const& child : node.children) {
         ImGui::TableNextRow();
         ImGui::TableNextColumn();
 
+        auto flags = &child == m_selected_node ? ImGuiTreeNodeFlags_Selected : ImGuiTreeNodeFlags_None;
+        bool open = false;
+
         if (!child.children.empty()) {
-            bool open = ImGui::TreeNodeEx(child.name.c_str());
-            if (open) {
-                traverse_model(child);
-                ImGui::TreePop();
-            }
-            continue;
+            open = ImGui::TreeNodeEx(child.name.c_str(), flags);
+        } else {
+            ImGui::TreeNodeEx(child.name.c_str(), flags | ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_Bullet | ImGuiTreeNodeFlags_NoTreePushOnOpen);
         }
 
-        ImGui::TreeNodeEx(child.name.c_str(), ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_Bullet | ImGuiTreeNodeFlags_NoTreePushOnOpen);
+        if (ImGui::IsItemClicked()) {
+            m_selected_node = &child;
+        }
+
+        if (open) {
+            traverse_model(child);
+            ImGui::TreePop();
+        }
     }
     ImGui::PopStyleColor(1);
 }
 
-void traverse_directory(FSCacheNode const& node)
+void AssetBrowser::traverse_directory(FSCacheNode const& node)
 {
     for (auto const& entry : node.children) {
         bool is_folder = entry.type == FSCacheNode::Type::DIRECTORY;
@@ -71,9 +77,24 @@ void traverse_directory(FSCacheNode const& node)
 void AssetBrowser::render()
 {
     ImGui::Begin("Asset Browser");
+    auto available_region = ImGui::GetContentRegionAvail();
+
+    ImGui::BeginChild("tree", ImVec2{available_region.x * 0.75f, available_region.y}, ImGuiChildFlags_ResizeX);
     if (ImGui::BeginTable("assetbrowser_directory_tree", 1)) {
         traverse_directory(*Project::get_current()->get_fs_cache());
         ImGui::EndTable();
     }
+    ImGui::EndChild();
+
+    ImGui::SameLine();
+
+    ImGui::BeginChild("preview");
+    if (m_selected_node) {
+        ImGui::Text("Preview placeholder for %s", m_selected_node->name.c_str());
+        // TODO: Preview nodes using ImGui::Image();
+    } else {
+        ImGui::Text("Preview placeholder");
+    }
+    ImGui::EndChild();
     ImGui::End();
 }
