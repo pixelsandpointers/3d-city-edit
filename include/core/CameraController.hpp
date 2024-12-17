38ff17cf7f4e14488af62459e6946c2a92d79923
Moritz Vogel <moritzv7@gmail.com>
2024-12-17 18:25:36 +0100

    Add global input handling

--- include/core/CameraController.hpp
+++ include/core/CameraController.hpp
@@ -10,7 +10,7 @@ struct CameraController {
         ORBIT,
     };
 
-    CameraController(GLFWwindow*, Type, glm::vec3 camera_position);
+    CameraController(Type, glm::vec3 camera_position);
     Type type;
     std::unique_ptr<Camera> camera;
 
@@ -20,18 +20,9 @@ struct CameraController {
     // Mouse sensitivity for looking around in radians / second
     float look_sensitivity{0.2f};
 
-    // TODO: Move window and key_pressed somewhere else
-    GLFWwindow* window;
-
     void update(float delta_time);
 
 private:
-    glm::dvec2 m_last_cursor_position;
-    glm::dvec2 m_cursor_delta;
-
     void update_freecam(float delta_time);
     void update_orbit(float delta_time);
-    void update_input();
-
-    bool key_pressed(int key);
 };
---
+++ src/core/Input.cpp
@@ -0,0 +1,50 @@
+#include "core/Input.hpp"
+
+GLFWwindow* Input::m_window;
+glm::dvec2 Input::m_last_cursor_position;
+glm::dvec2 Input::m_cursor_delta;
+glm::dvec2 Input::m_scroll_delta;
+
+void Input::init(GLFWwindow* window)
+{
+    m_window = window;
+    glfwSetScrollCallback(window, scroll_callback);
+}
+
+void Input::update()
+{
+    glm::dvec2 cursor_position;
+    glfwGetCursorPos(m_window, &cursor_position.x, &cursor_position.y);
+    m_cursor_delta = cursor_position - m_last_cursor_position;
+    m_last_cursor_position = cursor_position;
+}
+
+void Input::late_update()
+{
+    m_scroll_delta = glm::dvec2{0.0};
+}
+
+bool Input::key_pressed(int key)
+{
+    return glfwGetKey(m_window, key) == GLFW_PRESS;
+}
+
+bool Input::button_pressed(int button)
+{
+    return glfwGetMouseButton(m_window, button) == GLFW_PRESS;
+}
+
+glm::dvec2 Input::cursor_delta()
+{
+    return m_cursor_delta;
+}
+
+glm::dvec2 Input::scroll_delta()
+{
+    return m_scroll_delta;
+}
+
+void Input::scroll_callback(GLFWwindow*, double xoffset, double yoffset)
+{
+    m_scroll_delta = glm::dvec2{xoffset, yoffset};
+}
