543c73a98ede984522edfc1d9ef42eaa8f740e4e
Moritz Vogel <moritzv7@gmail.com>
2024-12-19 00:18:58 +0100

    Allow InstancedNode to be transformed independently of its Node

--- include/core/Scene.hpp
+++ include/core/Scene.hpp
@@ -9,17 +9,6 @@
 class Mesh;
 struct Node;
 
-// The model matrix needs to be cached because the performance would be awful otherwise.
-// However, caching the matrix in the `Node` itself would result in issues when the node is rendered multiple times.
-struct InstancedNode {
-    Node const* node;
-    glm::mat4 model_matrix{0.0f};
-    std::vector<InstancedNode> children;
-
-    void traverse(std::function<void(glm::mat4, Node const&)>) const;
-    void compute_transforms(glm::mat4 = glm::mat4{1.0f});
-};
-
 struct Transform {
     glm::vec3 position;
     glm::quat orientation;
@@ -28,6 +17,19 @@ struct Transform {
     [[nodiscard]] glm::mat4 get_local_matrix() const;
     [[nodiscard]] glm::vec3 orientation_euler() const;
     void set_orientation_euler(glm::vec3);
+};
+
+// The model matrix needs to be cached because the performance would be awful otherwise.
+// However, caching the matrix in the `Node` itself would result in issues when the node is rendered multiple times.
+// To be able to change the transform of each instances separately, InstancedNode needs its own transform component.
+struct InstancedNode {
+    Transform transform;
+    Node const* node;
+    glm::mat4 model_matrix{0.0f};
+    std::vector<InstancedNode> children;
+
+    void traverse(std::function<void(glm::mat4, Node const&)>) const;
+    void compute_transforms(glm::mat4 = glm::mat4{1.0f});
 };
 
 struct Node {
