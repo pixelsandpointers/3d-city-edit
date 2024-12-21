333b8ed26c7f2cc90ed855e6ae5e530b89a34a8a
Jonas Schröter <jonas.schroeter@stud.tu-darmstadt.de>
2024-12-21 21:34:21 +0100

    ui: adds object tree view

---
+++ include/ui/ObjectSelectionTree.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "core/Scene.hpp"
+struct ObjectSelectionTree {
+    ObjectSelectionTree();
+    void traverse_nodes(InstancedNode const& root);
+    void render(InstancedNode const& root);
+};
