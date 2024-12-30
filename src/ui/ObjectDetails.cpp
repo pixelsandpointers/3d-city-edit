346e2bb483ee4979c48d91c1bedb992e57d50e1c
Jonas Schröter <jonas.schroeter@stud.tu-darmstadt.de>
2024-12-30 18:25:54 +0100

    ui: precompute labels in ObjectDetails.cpp, changes InputText to InputFloat

--- src/ui/ObjectDetails.cpp
+++ src/ui/ObjectDetails.cpp
@@ -2,22 +2,19 @@
 #include <imgui.h>
 #include <string>
 
-ObjectDetails::ObjectDetails(){}
+ObjectDetails::ObjectDetails() = default;
 
 char object_label[128] = {""};
-
-char rotation[3][128] = {{"0"}, {"0"}, {"0"}};
-
-char scale[3][128] = { {"1"}, {"1"}, {"1"}};
-
-char position[3][128] = { {"0"}, {"0"}, {"0"} };
-
+float rotation[3][128] = {{0}, {0}, {0}};
+float scale[3][128] = {{1}, {1}, {1}};
+float position[3][128] = {{0}, {0}, {0}};
 bool toggle_shading = false;
 
-void ObjectDetails::render() {
-    ImVec2 min_size(300, 150);  // Base minimum size
-    ImVec2 max_size(FLT_MAX, FLT_MAX);  // No maximum size limit
-    const char* axis_labels[3] = { "X", "Y", "Z" };
+void ObjectDetails::render()
+{
+    ImVec2 min_size(300, 150); // Base minimum size
+    ImVec2 max_size(FLT_MAX, FLT_MAX); // No maximum size limit
+    char const* axis_labels[3] = {"X", "Y", "Z"};
 
     // Set the child window position to the right side of the parent window
     ImVec2 child_pos = ImVec2(0, 0);
@@ -25,40 +22,47 @@ void ObjectDetails::render() {
 
     // Apply size constraint before creating the window
     ImGui::SetNextWindowSizeConstraints(min_size, max_size);
-    ImGui::Begin("Object Details", nullptr, ImGuiWindowFlags_AlwaysAutoResize |ImGuiWindowFlags_NoMove);
+    ImGui::Begin("Object Details", nullptr, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoMove);
 
     ImGui::InputText("Label", object_label, IM_ARRAYSIZE(object_label));
+
+    char const* rotation_labels[] = {"X##rotation0", "Y##rotation1", "Z##rotation2"};
+    char const* scale_labels[] = {"X##scale0", "Y##scale1", "Z##scale2"};
+    char const* position_labels[] = {"X##position0", "Y##position1", "Z##position2"};
 
     // Rotation Row
     ImGui::Text("Rotation");
     for (int i = 0; i < 3; i++) {
-        ImGui::PushItemWidth(80);  // Set width for uniformity
-        ImGui::InputText((std::string(axis_labels[i]) + "##rotation" + std::to_string(i)).c_str(), rotation[i], IM_ARRAYSIZE(rotation[i]));
+        ImGui::PushItemWidth(100); // Set width for uniformity
+        ImGui::InputFloat(rotation_labels[i], rotation[i], 0.1f, 1.0f, "%.3f");
         ImGui::PopItemWidth();
-        if (i < 2) ImGui::SameLine();  // Keep on same row
+        if (i < 2)
+            ImGui::SameLine(); // Keep on same row
     }
 
     // Scale Row
     ImGui::Text("Scale");
     for (int i = 0; i < 3; i++) {
-        ImGui::PushItemWidth(80);
-        ImGui::InputText((std::string(axis_labels[i]) + "##scale" + std::to_string(i)).c_str(), rotation[i], IM_ARRAYSIZE(rotation[i]));
+        ImGui::PushItemWidth(100);
+        ImGui::InputFloat(scale_labels[i], scale[i], 0.1f, 1.0f, "%.3f");
         ImGui::PopItemWidth();
-        if (i < 2) ImGui::SameLine();
+        if (i < 2)
+            ImGui::SameLine();
     }
 
     // Position Row
     ImGui::Text("Position");
     for (int i = 0; i < 3; i++) {
-        ImGui::PushItemWidth(80);
-        ImGui::InputText((std::string(axis_labels[i]) + "##position" + std::to_string(i)).c_str(), rotation[i], IM_ARRAYSIZE(rotation[i]));
+        ImGui::PushItemWidth(100);
+        ImGui::InputFloat(position_labels[i], position[i], 0.1f, 1.0f, "%.3f");
         ImGui::PopItemWidth();
-        if (i < 2) ImGui::SameLine();
+        if (i < 2)
+            ImGui::SameLine();
     }
 
     if (ImGui::Checkbox("Shading", &toggle_shading)) {
         // add callback
-
     }
+
     ImGui::End();
 }
