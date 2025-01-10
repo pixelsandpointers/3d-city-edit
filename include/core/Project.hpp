640da48b6461438c46fe8e9391cef12313f8ed08
Moritz Vogel <moritzv7@gmail.com>
2025-01-10 16:58:45 +0100

    Add fs cache

--- include/core/Project.hpp
+++ include/core/Project.hpp
@@ -5,6 +5,20 @@
 #include <filesystem>
 #include <unordered_map>
 
+struct FSCacheNode {
+    enum class Type {
+        DIRECTORY,
+        MODEL,
+        TEXTURE,
+        OTHER,
+    };
+
+    std::filesystem::path path;
+    std::filesystem::file_time_type mtime;
+    Type type;
+    std::vector<FSCacheNode> children;
+};
+
 class Project {
 public:
     std::filesystem::path root;
@@ -12,14 +26,19 @@ public:
     static Project* get_current();
     static void load(std::filesystem::path);
 
+    FSCacheNode* get_fs_cache();
     Texture* get_texture(std::filesystem::path);
     Node* get_model(std::filesystem::path);
+    void rebuild_fs_cache();
+    void rebuild_fs_cache_timed(double current_time);
 
 private:
     static std::unique_ptr<Project> current;
 
     std::unordered_map<std::filesystem::path, Texture> m_textures;
     std::unordered_map<std::filesystem::path, Node> m_models;
+    std::unique_ptr<FSCacheNode> m_fs_cache;
+    double m_fs_cache_last_updated{0};
 
     Project(std::filesystem::path);
 };
