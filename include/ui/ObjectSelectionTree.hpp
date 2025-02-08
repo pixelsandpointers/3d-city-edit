534fad1410cd25abd179d07c3f1d35a826141753
Moritz Vogel <moritzv7@gmail.com>
2025-02-08 16:12:52 +0100

    Allow instantiating nodes between existing nodes

--- include/ui/ObjectSelectionTree.hpp
+++ include/ui/ObjectSelectionTree.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
 #include "core/Scene.hpp"
+#include <imgui_internal.h>
 
 struct ObjectSelectionTree {
     ObjectSelectionTree();
     void traverse_nodes(InstancedNode& root);
     void render();
+
+private:
+    std::optional<ImRect> m_prev_rect;
 };
--- src/ui/ObjectSelectionTree.cpp
+++ src/ui/ObjectSelectionTree.cpp
@@ -1,6 +1,7 @@
 #include "ui/ObjectSelectionTree.hpp"
 #include "core/Project.hpp"
-#include "imgui.h"
+#include <imgui.h>
+#include <imgui_internal.h>
 
 constexpr auto imgui_treenode_leaf_flags = ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_Bullet | ImGuiTreeNodeFlags_NoTreePushOnOpen;
 
@@ -35,6 +36,7 @@ void ObjectSelectionTree::traverse_nodes(InstancedNode& root)
             ImGui::TreeNodeEx(child.name.c_str(), imgui_treenode_leaf_flags | flags_selected);
         }
 
+        // Drop node onto TreeNode -> instantiate as child
         if (ImGui::BeginDragDropTarget()) {
             if (auto payload = ImGui::AcceptDragDropPayload("node")) {
                 auto node_to_instantiate = *static_cast<Node const**>(payload->Data);
@@ -43,6 +45,27 @@ void ObjectSelectionTree::traverse_nodes(InstancedNode& root)
             }
             ImGui::EndDragDropTarget();
         }
+
+        // Drop between TreeNodes
+        auto const current_rect = GImGui->LastItemData.Rect;
+        if (m_prev_rect.has_value()) {
+            auto const left_edge = std::min(current_rect.Min.x, m_prev_rect->Min.x);
+            auto const right_edge = std::max(current_rect.Max.x, m_prev_rect->Max.x);
+            auto const top_edge = m_prev_rect->Max.y;
+            auto const between_rect = ImRect{ImVec2{left_edge, top_edge}, ImVec2{right_edge, current_rect.Min.y}};
+
+            if (ImGui::BeginDragDropTargetCustom(between_rect, GImGui->LastItemData.ID)) {
+                // Drop node between TreeNodes -> instantiate as sibling
+                if (auto payload = ImGui::AcceptDragDropPayload("node")) {
+                    auto node_to_instantiate = *static_cast<Node const**>(payload->Data);
+                    child_it = root.children.insert(child_it, node_to_instantiate->instantiate());
+                    project->scene->compute_transforms();
+                }
+
+                ImGui::EndDragDropTarget();
+            }
+        }
+        m_prev_rect = current_rect;
 
         if (ImGui::IsItemClicked()) {
             project->selected_node = &child;
@@ -60,6 +83,7 @@ void ObjectSelectionTree::render()
     if (ImGui::Begin("Object Tree")) {
         if (auto& scene = Project::get_current()->scene; scene.has_value()) {
             if (ImGui::BeginTable("table0", 1)) {
+                m_prev_rect = {};
                 traverse_nodes(scene.value());
             }
             ImGui::EndTable();
