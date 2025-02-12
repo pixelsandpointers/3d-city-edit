feae355b743dc4282c3eb6047fb1568ab4e4e4aa
Jonas Schröter <jonas.schroeter@stud.tu-darmstadt.de>
2025-02-13 00:32:01 +0100

    moves ShaderUniformPane into SettingsPane.cpp, adds working Camera Mode setting, adds Fallback Texture Color Setting UI

--- include/ui/SettingsPane.hpp
+++ include/ui/SettingsPane.hpp
@@ -1,13 +1,16 @@
 #pragma once
 
+#include "core/CameraController.hpp"
 #include "renderer/Shader.hpp"
 #include <imgui.h>
 #include <unordered_map>
 
-struct ShaderUniformPane {
+struct SettingsPane {
     void render();
 
     bool draw_wireframe = false;
+    glm::vec3 fallback_color{255, 255, 255};
     Uniforms uniforms{};
     ViewingMode viewing_mode = ViewingMode::RENDERED;
+    CameraController::Type camera_type = CameraController::Type::FREECAM;
 };
