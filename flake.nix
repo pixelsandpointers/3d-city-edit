a736d4f78e56f357727647cacc2ded0cb3e5ecd8
Moritz Vogel <moritzv7@gmail.com>
2025-03-05 14:53:23 +0100

    Add library ImGuizmo

--- flake.nix
+++ flake.nix
@@ -19,22 +19,46 @@
           name = "3d";
           src = ./.;
           nativeBuildInputs = with pkgs; [ cmake ninja pkg-config ];
-          buildInputs = with pkgs; [
-            assimp
-            glm
-            glfw
-            ((imgui.overrideAttrs rec {
+          buildInputs = let
+            imgui = (pkgs.imgui.overrideAttrs rec {
               version = "1.91.5-docking";
-              src = fetchFromGitHub {
+              src = pkgs.fetchFromGitHub {
                 owner = "ocornut";
                 repo = "imgui";
                 tag = "v${version}";
                 hash = "sha256-6VOs7a31bEfAG75SQAY2X90h/f/HvqZmN615WXYkUOA=";
               };
-            }).override { IMGUI_BUILD_GLFW_BINDING = true; })
-            zlib
-            nlohmann_json
-          ];
+            }).override { IMGUI_BUILD_GLFW_BINDING = true; };
+
+            imguizmo = let
+              vcpkgSource = pkgs.fetchFromGitHub {
+                owner = "microsoft";
+                repo = "vcpkg";
+                rev = "4110d43398232f486c3fd0b74e2dc9e4ca5e2a59";
+                hash = "sha256-fqNuZRrdtpPFIU3+N7qSG/Z1QRpBBdCAm7wFe05fPmA=";
+                sparseCheckout = [ "ports/imguizmo" ];
+              };
+            in pkgs.stdenv.mkDerivation {
+              pname = "imguizmo";
+              version = "unstable-2024-11-14";
+
+              src = pkgs.fetchFromGitHub {
+                owner = "CedricGuillemet";
+                repo = "ImGuizmo";
+                rev = "b10e91756d32395f5c1fefd417899b657ed7cb88";
+                hash = "sha256-nOMapXR2Gzz0yg5qqf0JW44JbwPkrLFxlZYQOK5dQnM=";
+              };
+
+              cmakeRules = "${vcpkgSource}/ports/imguizmo";
+              postPatch = ''
+                cp "$cmakeRules"/CMakeLists.txt ./
+              '';
+
+              nativeBuildInputs = [ pkgs.cmake ];
+              buildInputs = [ imgui ];
+            };
+          in with pkgs;
+          [ assimp glm glfw zlib nlohmann_json ] ++ [ imgui imguizmo ];
         };
       });
 
